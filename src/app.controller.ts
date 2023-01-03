a2661b7ff3bc92816cc63caaffa17e3972e8ad9e
joaopedroaquino <aqerhack@gmail.com>
2023-01-03 11:41:26 -0300

    init app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  private logger = new Logger(AppController.name)
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  list(): string {
+     return 
+ }
+
+  create(): string {
+     return  
+ }
+
 }
