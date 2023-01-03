d3ce8edfc3a6e0a6c582d808d927b800ec57eb48
joaopedroaquino <aqerhack@gmail.com>
2023-01-03 11:32:05 -0300

    create user entity

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from 'src/users/database/user.entity';
+import { UserEntity } from 'src/interfaces/user.entity';
 
 @Module({
     imports: [
