1858de8d33fb5f33d84921942986d75910903716
joaopedroaquino <aqerhack@gmail.com>
2023-01-03 11:42:58 -0300

    create get route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get, Logger } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
+import { UserEntity } from './interfaces/user.entity';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
   private logger = new Logger(AppController.name)
 
-  list(): string {
-     return 
+  @MessagePattern('find-all-user')
+  async list(): Promise<UserEntity[]> {
+     return this.appService.list()
  }
 
   create(): string {
